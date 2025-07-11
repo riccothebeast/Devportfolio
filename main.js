5f83fa1ee7a3a92e2b034cf10cd32e500ceb69e3
riccothebeast <robbinithai@gmail.com>
2025-07-11 13:13:51 +0300

    My portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",
+    {
+    strings: ["Web Developer", "UI/UX Designer", "Freelancer"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 70,
+    loop: true
+  });
+
